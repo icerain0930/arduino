e442dc0cf13ccffd90ba23d97522ac4a7f9e338f
icerain0930 <62783196+icerain0930@users.noreply.github.com>
2022-03-05 22:43:07 +0800

    七段顯示器
    0 > 1 > 2 >3 > 4 > 5 > 6 > 7 > 8 > 9 (loop)

---
+++ 7-1.ino
@@ -0,0 +1,30 @@
+bool d[10][7]={{0,0,0,0,0,0,1},
+{1,0,0,1,1,1,1},
+{0,0,1,0,0,1,0},
+{0,0,0,0,1,1,0},
+{1,0,0,1,1,0,0},
+{0,1,0,0,1,0,0},
+{0,1,0,0,0,0,0},
+{0,0,0,1,1,1,1},
+{0,0,0,0,0,0,0},
+{0,0,0,1,1,0,0}};
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  digitalWrite(9,1);
+}
+
+void loop() {
+  for(int i=0;i<10;i++){
+    for(int j=0;j<7;j++){
+      digitalWrite(j+2,d[i][j]);
+    } 
+    delay(1000);
+  }
+}
